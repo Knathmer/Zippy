fff4de429e0478e3e4708f2de9f49863ce6afefb
Eraj <anwaraeraj@gmail.com>
2025-03-31 17:15:03 -0500

    Unrar Implementation

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
-from pystray import Icon, MenuItem, Menu
-from PIL import Image
+# Standard Library
+import platform
 import time
+import os
 
-def exit_action(icon, item):
+#Environment Variable Set
+os.environ["UNRAR_LIB_PATH"] = r"C:\Users\18324\Documents\VSCodeProjects\Zippy\unrar.dll" # Environment Variable for unrar
+
+# Third-Party Library
+from pystray import Icon, MenuItem as item, Menu
+from PIL import Image
+from unrar import rarfile
+
+# Functions
+def close_zippy(icon, item): 
     icon.stop()
+def testFunction():
+    print("Button pressed!")
 
-image = Image.new('RGB', (64, 64), (255, 0, 0)) # Creates the image for the tray
-menu = Menu(MenuItem('Exit', exit_action)) # Creates
-icon = Icon("test_icon", image, "My Tray App", menu)
+
+image = Image.open("icons/zippy.png") # Creates the image for the tray
+
+
+menu = Menu(
+        item('Exit', close_zippy), 
+        item(
+            'File Locations',
+            Menu(
+                item(
+                    'Location 1',
+                    testFunction),
+                item(
+                    'Location 2',
+                    testFunction))))
+
+
+# Constructing the entire menu
+icon = Icon("test_icon", image, "Zippy", menu)
+
+
+
+
+
+
+
+#rar = rarfile.RarFile('Industry Resumes.rar')
+#print(rar.namelist())
+
+
 
 icon.run()
