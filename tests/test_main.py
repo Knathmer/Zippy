18c82826628b18450e29c9b223e4281ce5dae87f
Eraj <anwaraeraj@gmail.com>
2025-07-10 09:53:27 -0500

    main and config tests

---
+++ tests/test_main.py
@@ -0,0 +1,23 @@
+import pytest
+import zippy.main
+from zippy.ui import close_zippy
+import threading
+import time
+
+def test_main():
+
+    icon = None
+    def run_icon():
+        nonlocal icon
+        icon = zippy.main.main(run_in_thread=True)
+
+    t = threading.Thread(target=run_icon)
+    t.start()
+    time.sleep(4)
+
+    assert icon is not None
+
+    if icon:
+        icon.stop()
+
+    t.join(timeout=3)
--- zippy/main.py
+++ zippy/main.py
@@ -1,9 +1,25 @@
 # zippy/main.py
 from .ui import create_tray
+import threading
+import time as t
 
-def main():
-    icon = create_tray()
-    icon.run()
+def main(run_in_thread=False):
+
+    icon = None
+    def create_and_run_icon():
+        nonlocal icon
+        icon = create_tray()
+        icon.run()  # Blocking call, runs the icon event loop
+
+    if not run_in_thread:
+        create_and_run_icon()
+        return True
+    else:
+        print("Running icon in a separate thread")
+        icon_thread = threading.Thread(target=create_and_run_icon, daemon=True)
+        icon_thread.start()
+        t.sleep(4)
+        return icon
 
 if __name__ == "__main__":
     main()
